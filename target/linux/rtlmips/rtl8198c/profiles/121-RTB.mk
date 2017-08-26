fb773facd767a674af7ed2cacf7124a9f2eff5e4
fengmushu <fengmushu@gmail.com>
2017-08-26 12:16:59 +0800

    openwrt for rtl8198c merged

---
+++ target/linux/rtlmips/rtl8198c/profiles/121-RTB.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2006-2008 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/RTB
+  NAME:=Router Boards package
+  PACKAGES:=
+endef
+
+define Profile/RTB/Description
+	Realtek SOC,Package AP mode support
+endef
+
+$(eval $(call Profile,RTB))
